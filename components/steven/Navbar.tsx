003ba1ae77fe710860f5072577db5d14857a3bc3
smfranci <smfranci@stud.ntnu.no>
2021-11-05 01:45:52 +0100

    added navbar to /steven pages

---
+++ components/steven/Navbar.tsx
@@ -0,0 +1,53 @@
+import { FC } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "@/styles/components/steven/NavBar.module.scss";
+
+/**
+ * The Navbar component used on all pages of the application.
+ * @return {JSX.Element} The JSX code for Navbar component.
+ */
+const Navbar: FC = (): JSX.Element => {
+  const { user } = useUser();
+
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <Link href="/steven">Home</Link>
+        </li>
+        <li>
+          <Link href="/steven/about">About</Link>
+        </li>
+        <li>
+          <Link href="/steven/pokemon">Pokemon</Link>
+        </li>
+        <div className={styles.navbarRight}>
+          {!user ? (
+            <li>
+              <Link href="/api/auth/login">Login</Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link href="/api/auth/logout">Logout</Link>
+              </li>
+              {user.picture && (
+                <Image
+                  src={user.picture}
+                  alt="profile-picture"
+                  width={30}
+                  height={30}
+                />
+              )}
+            </>
+          )}
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/steven/index.tsx
+++ pages/steven/index.tsx
@@ -4,6 +4,7 @@ import Button from "@/components/Button";
 
 import styles from "@/styles/pages/steven/Steven.module.scss";
 import UtilFunctions from "@/utils/functions";
+import Navbar from "@/components/steven/Navbar";
 
 /**
  * Steven's main page
@@ -12,6 +13,7 @@ import UtilFunctions from "@/utils/functions";
 const Steven: FC = (): JSX.Element => {
   return (
     <div className={styles.steven}>
+      <Navbar />
       <p>Hello my name is Steven😄</p>
       <Button
         text={"POKEMON"}
